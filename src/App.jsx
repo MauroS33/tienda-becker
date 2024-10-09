4ecbf84aff6de03bbc8dfb1491829157348e2e11
MauroS33 <maurosotelozicari@gmail.com>
2024-10-08 21:24:05 -0300

    [M] correccion de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './assets/ItemListContainer'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import './App.css'
 
 
