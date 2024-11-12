95f9cc8f5817396be438049c7a3404beff6a949d
MauroS33 <maurosotelozicari@gmail.com>
2024-11-12 20:51:24 -0300

    [M] solucionado imagenes en home

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Item = ({product}) => {
   return (
     <div className='item'>
-      <img src={product.image[0]} className="img-item" alt="item" />
+      <img src={product.image} className="img-item" alt="item" />
       <p className="text-item"> {product.name} </p>
       <p className="text-item"> {product.price} </p>
 
